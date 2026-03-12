66c9e4120fbe996cced5a13b52de95af722d4d0a
Prachi Vaidya <prachinvaidya20@gmail.com>
2026-03-12 22:06:02 +0530

    Create load_bronze.sql

---
+++ scripts/bronze/load_bronze.sql
@@ -0,0 +1,7 @@
+/*
+-----------------------------------------------------------------------------------------
+I preferred the **IMPORT/EXPORT** option for loading the dataset into PostgreSQL,
+because the dataset is not huge so, I found this method easier for loading 
+the data into the tables.
+-----------------------------------------------------------------------------------------
+*/
